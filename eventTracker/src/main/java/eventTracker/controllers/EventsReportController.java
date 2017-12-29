5488d882d2e80589cbfc0de548f77d0b0a7fb862
youss <youss.hark@gmail.com>
2017-12-29 16:31:40 +0100

    first and last commit

---
+++ eventTracker/src/main/java/eventTracker/controllers/EventsReportController.java
@@ -0,0 +1,28 @@
+package eventTracker.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import eventTracker.model.Event;
+
+@RestController
+public class EventsReportController {
+	
+	@RequestMapping("/events")
+	public List<Event> getEvents(){
+		
+		List<Event> events = new ArrayList<>();
+		Event event1 = new Event();
+		event1.setName("Java User Group");
+		Event event2 = new Event();
+		event2.setName("Angular User Group");
+		events.add(event1);
+		events.add(event2);
+		
+		return events;
+	}
+
+}
